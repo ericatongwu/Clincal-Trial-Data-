63dd14eaad16bae6a1b641061503cb5b52e90c8c
Erica Wu <twu665@usc.edu>
2019-02-01 13:46:25 -0800

    update json

--- extractDrugName.py
+++ extractDrugName.py
@@ -8,8 +8,15 @@ from nltk.tokenize import word_tokenize
 
 # Read in the Interventions column of the file
 loc = ("0 2013-2017 Working.Inter_Drugs_Resp P_Ph_Spon_F By_Loc_45897 (201805.30.).xlsx")
+disease = ("trials_classified2018.xlsx")
+
 wb = xlrd.open_workbook(loc)
+disease_open = xlrd.open_workbook(disease)
+
 sheet = wb.sheet_by_index(1)
+disease_sheet = disease_open.sheet_by_index(0)
+
+
 all_drug = {}
 # Create a corpus of stopwords
 stopwords = ['','plus','alone','vaccine','matching','with','placebo','to','of','pills',
@@ -20,7 +27,7 @@ stopwords = ['','plus','alone','vaccine','matching','with','placebo','to','of','
 				'test','product','reference','ca','irvine','stem','cells','matching','low',
 				'range','the','inc','cell','for','a','b','injection','protocol','placebos','oral'
 				'fix','fixed','side','effect','level','taken','every','minutes','or','ml']
-def get_data(csv_sheet):
+def get_data(sheet1, sheet2):
 
 	# Process elements in each row
 	for i in range(1,45897): 
@@ -40,19 +47,27 @@ def get_data(csv_sheet):
 						all_drug[NCT] = {"Biological": value[i+1:]}
 			else:
 				all_drug.pop(NCT, None)
+	
+	for i in range(1,7465):
+		for nct_number in word_tokenize(disease_sheet.cell_value(i, 2)):
+			disease_kind = {"disease":[disease_sheet.cell_value(i, 7),disease_sheet.cell_value(i, 28)]}
+			unknown = {"disease": "Do not know yet"}
+			dict(all_drug[nct_number].items() + disease_kind.items())
 
 	with open('result.json', 'w') as fp:
 	    json.dump(all_drug, fp)
 
 	return all_drug
+	
 
-	# druglist = [d for d in druglist if 'Drug' in d or 'Biological' in d]
-	# currDrug = ' '.join(druglist)
-	# druglist = re.split('\s|\/|\,|\(|\)|\%|',currDrug)
+print(get_data(sheet, disease_sheet))
 
-	# druglist = [d for d in druglist if d.lower() not in stopwords]
-	#print(''.join(druglist))
-	#for line in ''.join(druglist):
 
-print(get_data(sheet))
+# druglist = [d for d in druglist if 'Drug' in d or 'Biological' in d]
+# currDrug = ' '.join(druglist)
+# druglist = re.split('\s|\/|\,|\(|\)|\%|',currDrug)
+
+# druglist = [d for d in druglist if d.lower() not in stopwords]
+#print(''.join(druglist))
+#for line in ''.join(druglist):
 
